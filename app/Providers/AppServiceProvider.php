1da09f58ecf3b53340c5fc7e032ea83d7781e0cb
paapladspjuske@gmail.com <10414028@ucn.dk>
2025-06-06 20:53:42 +0200

    https force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         $this->app->scoped(\App\Client\IMovieClient::class,\App\Client\MovieClient::class);
         $this->app->scoped(\App\Client\ITmdbClient::class,\App\Client\TmdbClient::class);
     }
