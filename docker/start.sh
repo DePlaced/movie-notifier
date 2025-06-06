b6194e4a4494708c2dccb50b04656fe7d12c1efa
paapladspjuske@gmail.com <10414028@ucn.dk>
2025-06-06 20:39:13 +0200

    start

--- docker/start.sh
+++ docker/start.sh
@@ -3,5 +3,5 @@ set -e
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate --seed --force
+php artisan migrate --force
 exec /usr/bin/supervisord -c /etc/supervisord.conf
