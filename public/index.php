867ad45b216142680b6c2103a83eed46ef0378a0
paapladspjuske@gmail.com <10414028@ucn.dk>
2025-06-06 17:40:44 +0200

    docker

--- public/index.php
+++ public/index.php
@@ -1,10 +1,8 @@
 <?php
 
-if (env('APP_ENV') !== 'production') {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-}
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
