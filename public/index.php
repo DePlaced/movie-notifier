f0dfe8d98bd0740e249923867eeabd6d33d513e2
paapladspjuske@gmail.com <10414028@ucn.dk>
2025-06-06 17:32:51 +0200

    index debug

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
+if (env('APP_ENV') !== 'production') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
